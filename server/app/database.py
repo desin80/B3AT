4fa0bd4405872da465caab15c5cb189496be8a61
desin <ahuman233@outlook.com>
2025-12-06 09:18:35 +0800

    enable WAL

--- server/app/database.py
+++ server/app/database.py
@@ -4,6 +4,7 @@ from .config import DB_PATH
 
 def get_db_connection():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+    conn.execute("PRAGMA journal_mode=WAL;")
     conn.row_factory = sqlite3.Row
     return conn
 
